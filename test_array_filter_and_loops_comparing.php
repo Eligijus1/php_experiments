514561b631b043b98f5e06af8b0c510d831ca3da
Eligijus Stugys <eligijus.stugys@nfq.lt>
2017-01-26 10:16:48 +0200

    Updated loop experiments.

--- test_array_filter_and_loops_comparing.php
+++ test_array_filter_and_loops_comparing.php
@@ -1,4 +1,8 @@
 <?php
+/*
+execute this from console by typing: php test_array_filter_and_loops_comparing.php
+*/
+
 ini_set('memory_limit', '500M');
 $data = range(0, 1000000);
 
@@ -11,6 +15,9 @@ $end = microtime(true);
  
 echo "\narray_filter loop: ";
 echo $end - $start;
+echo "\tsec.";
+echo "\t";
+echo echo_memory_usage();
  
 // Foreach loop average 0.37 seconds
 $start = microtime(true);
@@ -24,6 +31,9 @@ $end = microtime(true);
  
 echo "\nForeach loop:      ";
 echo $end - $start;
+echo "\tsec.";
+echo "\t";
+echo echo_memory_usage();
 
 // For loop average 0.61 seconds
 $start = microtime(true);
@@ -38,6 +48,9 @@ $end = microtime(true);
 
 echo "\nFoor loop:         ";
 echo $end - $start;
+echo "\tsec.";
+echo "\t";
+echo echo_memory_usage();
 
 // While loop average 0.58 seconds
 $start = microtime(true);
@@ -54,4 +67,25 @@ $end = microtime(true);
  
 echo "\nWhile loop:        ";
 echo $end - $start;
-echo "\n";
+echo "\tsec.";
+echo "\t";
+echo echo_memory_usage();
+
+// Last new line:
+echo "\n\n";
+
+/**
+ * Print memory usage.
+ */
+function echo_memory_usage() { 
+    $mem_usage = memory_get_usage(true); 
+    
+    if ($mem_usage < 1024) 
+        echo $mem_usage." bytes"; 
+    elseif ($mem_usage < 1048576) 
+        echo round($mem_usage/1024,2)." kilobytes"; 
+    else 
+        echo round($mem_usage/1048576,2)." megabytes"; 
+} 
+
+
