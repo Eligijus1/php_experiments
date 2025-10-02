1475223ff104e5bbb92108078258a6466988517f
Eligijus Stugys <eligijus.stugys@gmail.com>
2025-10-02 09:08:34 +0300

    ElasticsearchClient_initial_version

---
+++ Elasticsearch/ElasticsearchClient.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Elasticsearch;
+
+use Throwable;
+
+class ElasticsearchClient
+{
+    private $elasticClient;
+
+    public function __construct(string $elasticCloudWebUiCloudId, string $elasticCloudWebUiApiId, string $elasticCloudWebUiApiKey)
+    {
+        $this->elasticClient = ClientBuilder::create()
+            ->setElasticCloudId($elasticCloudWebUiCloudId)
+            ->setApiKey($elasticCloudWebUiApiId, $elasticCloudWebUiApiKey)
+            ->build();
+    }
+
+    public function searchDocumentsInIndexWithSql(string $sql): array
+    {
+        $params = [
+            'body' => [
+                'query' => $sql
+            ]
+        ];
+
+        try {
+            $response = $this->elasticClient->sql()->query($params);
+        } catch (Throwable $e) {
+            $this->logException($e);
+            echo("Failed SQL: $sql");
+            return [];
+        }
+
+        return $response;
+    }
+}
